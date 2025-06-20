4927a3e3f9daeabac26a280cbd2bbb7965482e5a
Amit-Yadav56 <139137290+Amit-Yadav56@users.noreply.github.com>
2025-06-20 11:24:46 +0530

    Landing page created without animations

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -9,30 +9,30 @@ const LandingPage = () => {
           Transform your skincare routine with premium products that restore,
           protect, and enhance your nautural glow every day.
         </div>
-        <div className="font-bold text-[100px] leading-[90px] tracking-[-0.03em] uppercase">
+        <div className="font-bold text-[5vw] leading-[90px] tracking-[-0.03em] uppercase">
           GLOW <br /> NATUR- <br />
           ALLY
         </div>
         <img src={skinCareCream} className="w-[15%]" alt="cream" />
       </div>
       {/*Section 2*/}
-      <div className="flex justify-center items-center relative h-[676px]">
-        <div className="absolute inset-0 flex flex-col justify-end items-center z-0">
-          <div className="w-full">
-            <button className="bg-black text-white px-6 py-2 rounded">
+      <div className="w-full h-screen relative flex justify-center items-end">
+        <div className="absolute inset-0 z-0 flex flex-col justify-end items-center px-4 gap-20">
+          <div className="w-full flex justify-start">
+            <button className="bg-[#2D3B36] text-white px-6 py-2 rounded-[100px] mt-4 w-[180px] h-[60px] cursor-pointer">
               Shop Now
             </button>
           </div>
-          <p className="font-extrabold text-[378px] leading-[100%] tracking-[0]">
+          <p className="w-full text-center text-[20.7vw] leading-[0.73] font-semibold relative left-[-7.7%] text-[#2D3B36]">
             SKINCARE
           </p>
         </div>
 
-        {/* Center image that pops out */}
+        {/* Foreground image */}
         <img
           src={img1}
           alt=""
-          className="h-[676px] w-[610px] rounded-[30px] object-cover z-10 shadow-xl mb-5"
+          className="w-[30vw] rounded-[30px] object-cover z-10 shadow-xl h-[50vh] mb-5"
         />
       </div>
     </div>
